d2674a0f670184584db961de9bb40e5522a17eeb
Pavindu Lakshan <pavindu@wso2.com>
2022-11-17 06:07:36 +0530

    Modify branding

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'IAM Developer Docs',
+  tagline: 'Developer Documentation',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -57,10 +57,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'IAM Developer Docs',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'IAM Developer Docs',
+          src: 'img/wso2-logo.png',
         },
         items: [
           {
@@ -71,10 +71,14 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/pavinduLakshan/docusaurus-poc',
             label: 'GitHub',
             position: 'right',
           },
+          {
+            type: 'search',
+            position: 'right'
+          }
         ],
       },
       footer: {
@@ -125,6 +129,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['java']
       },
     }),
 };
